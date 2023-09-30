e7bd86ee7ba2f00c62bc5e58476e8ce50de4bd1f
Nico Krause <nico@le-space.de>
2023-09-30 21:38:59 +0500

    implemented basic ui

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,57 +1,28 @@
 <script lang="ts">
-	import { onMount, onDestroy } from 'svelte';
-	import { fromString as uint8ArrayFromString } from 'uint8arrays/from-string';
-	import { UnixFS } from 'ipfs-unixfs';
+	import { onMount } from 'svelte';
+	import {TextInput, Button, DataTable, Grid, Row, Column, Tile} from "carbon-components-svelte";
 
-	let ipfsNode1,ipfsNode2;;
+	let ipfsNode1,ipfsNode2;
 	let nodeId;
+	let orbitdb1;
 	let putted, added;
 	let putWithLinks, addedWithLinks;
 
-	let helloWorld = {
-		hello: 'world'
-	};
+	let myAddressBook = "/orbitdb/zdpuAqSriPZbh2PWy1m5d7n1ZfzQPPmnGqMM8KGYxBHWgqXEt" //"myAddressBook";
+	let myAddressBookDB;
 
+	let allMyAddressFields = []
+	let allMyContacts = []
+	let currentAddress = {}
 	onMount(async () => {
 		const config1 = {
-			// Addresses: {
-			// 	API: '/ip4/127.0.0.1/tcp/0',
-			// 	Swarm: ['/ip4/0.0.0.0/tcp/0'],
-			// 	Gateway: '/ip4/0.0.0.0/tcp/0'
-			// },
 			Bootstrap: [],
 			Addresses: {
 				Swarm: [
-					// '/dns4/ipfs.le-space.de/tcp/4003/wss/p2p/12D3KooWALjeG5hYT9qtBtqpv1X3Z4HVgjDrBamHfo37Jd61uW1t',
-					// '/dns6/ipfs.le-space.de/tcp/4003/wss/p2p/12D3KooWALjeG5hYT9qtBtqpv1X3Z4HVgjDrBamHfo37Jd61uW1t'
-					// '/dns4/ws-star.discovery.libp2p.io/tcp/443/wss/p2p-websocket-star',
-					// '/dns4/ipfs.infura.io/tcp/443/wss/p2p-webrtc-star',
-					// '/p2p-circuit/ipfs/12D3KooWALjeG5hYT9qtBtqpv1X3Z4HVgjDrBamHfo37Jd61uW1t',
 					'/dns6/ipfs.le-space.de/tcp/9091/wss/p2p-webrtc-star',
 					'/dns4/ipfs.le-space.de/tcp/9091/wss/p2p-webrtc-star'
 				],
 			},
-			Discovery: {
-				MDNS: {
-					Enabled: true,
-					Interval: 0
-				}
-			}
-		}
-
-		const config2 = {
-			Addresses: {
-				Swarm: [
-					// '/dns4/ipfs.le-space.de/tcp/4003/wss/p2p/12D3KooWALjeG5hYT9qtBtqpv1X3Z4HVgjDrBamHfo37Jd61uW1t',
-					// '/dns6/ipfs.le-space.de/tcp/4003/wss/p2p/12D3KooWALjeG5hYT9qtBtqpv1X3Z4HVgjDrBamHfo37Jd61uW1t'
-					// '/dns4/ws-star.discovery.libp2p.io/tcp/443/wss/p2p-websocket-star',
-					// '/dns4/ipfs.infura.io/tcp/443/wss/p2p-webrtc-star',
-					// '/p2p-circuit/ipfs/12D3KooWALjeG5hYT9qtBtqpv1X3Z4HVgjDrBamHfo37Jd61uW1t',
-					'/dns6/ipfs.le-space.de/tcp/9091/wss/p2p-webrtc-star',
-					'/dns4/ipfs.le-space.de/tcp/9091/wss/p2p-webrtc-star'
-				],
-			},
-			Bootstrap: [],
 			Discovery: {
 				MDNS: {
 					Enabled: true,
@@ -67,11 +38,9 @@
 			const IPFSmodule = await import('../modules/ipfs-core/ipfs-core.js');
 			const IPFS = IPFSmodule.default;
 			ipfsNode1 = await IPFS.create({ config: config1, repo: './ipfs/1' });
-			ipfsNode2 = await IPFS.create({ config: config2, repo: './ipfs/2' });
 			globalThis.ipfsNode1 = ipfsNode1;
 		} else {
 			ipfsNode1 = globalThis.ipfsNode1;
-			ipfsNode2 = globalThis.ipfsNode2;
 		}
 
 		const identity = await ipfsNode1.id();
@@ -80,65 +49,55 @@
 		const OrbitDBModul = await import('../modules/orbitdb-core/index.js');
 		const createOrbitDB = OrbitDBModul.createOrbitDB;
 		// const ipfsAccessController = OrbitDBModul.IPFSAccessController;
-		const orbitdb1 = await createOrbitDB({ ipfs: ipfsNode1 })
-		const orbitdb2 = await createOrbitDB({ ipfs: ipfsNode2 })
+		orbitdb1 = await createOrbitDB({ ipfs: ipfsNode1 })
 
-		//1. Create my own addressDB and receive a DAL (decentralized address link)
-		const aliceDAL = "aliceDAL"
-		const aliceDALDB = await orbitdb1.open(aliceDAL, { type: 'keyvalue' })
-		await aliceDALDB.put('NAME', "Alice Brown")
-		await aliceDALDB.put('CITY', "Istanbul")
-		const allAlice = await aliceDALDB.all()
-		console.log("allAlice",allAlice)
-
-		//2. Bobs DAL
-		const bobDAL = "bobDAL"
-		const bobDALDB = await orbitdb2.open(bobDAL, { type: 'keyvalue' })
-		await bobDALDB.put('NAME', "Bob Smith")
-		await bobDALDB.put('STREET', "The Address Downtown")
-		await bobDALDB.put('CITY', "Dubai")
-		const allBob = await bobDALDB.all()
-		console.log("allBob",allBob)
+		// //2. Bobs DAL
+		// const bobDAL = "bobDAL"
+		// const bobDALDB = await orbitdb2.open(bobDAL, { type: 'keyvalue' })
+		// await bobDALDB.put('NAME', "Bob Smith")
+		// await bobDALDB.put('STREET', "The Address Downtown")
+		// await bobDALDB.put('CITY', "Dubai")
+		// const allBob = await bobDALDB.all()
+		// console.log("allBob",allBob)
 
 		//3. My address book
-		const aliceAddressBook = "myAddressBook"
-		const aliceAddressBookDB = await orbitdb1.open(aliceAddressBook, { type: 'keyvalue' })
-		await aliceAddressBookDB.put('MY-DAL', aliceDALDB.address)
-		const bobsName = await bobDALDB.get("NAME")
-		let allAliceaddressbook = await aliceAddressBookDB.all()
-		console.log("allAliceaddressbook",allAliceaddressbook)
+
+
+		// const bobsName = await bobDALDB.get("NAME")
+		// let allAliceaddressbook = await myAddressBookDB.all()
+		// console.log("allAliceaddressbook",allAliceaddressbook)
 
 		//4. Bobs address book (has only his address
 		// const myAddressBook = "myAddressBook"
-		const bobsAddressBookDB = await orbitdb2.open(aliceAddressBook, { type: 'keyvalue' })
-		await bobsAddressBookDB.put('MY-DAL', bobDALDB.address)
-		const allbobsAddressBookDB = await bobsAddressBookDB.all()
-		console.log("allbobsAddressBookDB",allbobsAddressBookDB)
+		// const bobsAddressBookDB = await orbitdb2.open(myAddressBook, { type: 'keyvalue' })
+		// await bobsAddressBookDB.put('MY-DAL', bobDALDB.address)
+		// const allbobsAddressBookDB = await bobsAddressBookDB.all()
+		// console.log("allbobsAddressBookDB",allbobsAddressBookDB)
 
 		//Now Alice is connecting (scanning) Bobs address book (we need his address not his contacts!)
 		//TODO who gets read access for bobs address book? only bob or everybody with a password or privatekey?
-		const alice2BobsDAL = await orbitdb1.open(bobDALDB.address, { type: 'keyvalue' })
-		const allalice2BobsDAL = await alice2BobsDAL.all()
-		console.log("allalice2BobsDAL",allalice2BobsDAL) //alice orbitdb now contains bobs address
+		// const alice2BobsDAL = await orbitdb1.open(bobDALDB.address, { type: 'keyvalue' })
+		// const allalice2BobsDAL = await alice2BobsDAL.all()
+		// console.log("allalice2BobsDAL",allalice2BobsDAL) //alice orbitdb now contains bobs address
 
 		//we now put bobs dal book into alice addressbook and iterator over all entries in alice address book and print out all DALs
-		await aliceAddressBookDB.put(bobsName, bobDALDB.address)
-		allAliceaddressbook = await aliceAddressBookDB.all()
-		console.log("allAliceaddressbook",allAliceaddressbook)
+		// await myAddressBookDB.put(bobsName, bobDALDB.address)
+		// allAliceaddressbook = await myAddressBookDB.all()
+		// console.log("allAliceaddressbook",allAliceaddressbook)
 
 		//now change bobs address (Bob is moving to a new address)
-		const newAddress = "Jumeirah Beach"
-		console.log("changing bobs address",newAddress)
-		await bobDALDB.put('STREET',newAddress)
+		// const newAddress = "Jumeirah Beach"
+		// console.log("changing bobs address",newAddress)
+		// await bobDALDB.put('STREET',newAddress)
 
-		for (let key in allAliceaddressbook) {
-			console.log("obj",allAliceaddressbook[key])
-			const dbAddr = allAliceaddressbook[key].value
-			console.log(`connecting to ${allAliceaddressbook[key].key}'s ${dbAddr} `)
-			const syncingDAL = await orbitdb1.open(dbAddr, { type: 'keyvalue' })
-			const allSyncingDAL = await syncingDAL.all()
-			console.log("allSyncingDAL",allSyncingDAL)
-		}
+		// for (let key in allAliceaddressbook) {
+		// 	console.log("obj",allAliceaddressbook[key])
+		// 	const dbAddr = allAliceaddressbook[key].value
+		// 	console.log(`connecting to ${allAliceaddressbook[key].key}'s ${dbAddr} `)
+		// 	const syncingDAL = await orbitdb1.open(dbAddr, { type: 'keyvalue' })
+		// 	const allSyncingDAL = await syncingDAL.all()
+		// 	console.log("allSyncingDAL",allSyncingDAL)
+		// }
 
 		return () => {
 			console.log('the ipfs node is being stopped');
@@ -148,88 +107,105 @@
 			globalThis.ipfsNode2 = null;
 		};
 	});
-</script>
 
-<svelte:head>
-	<title>Home</title>
-</svelte:head>
+	const connect = async () => {
+		console.log("connecting to ",myAddressBook)
+		myAddressBookDB = await orbitdb1.open(myAddressBook, { type: 'keyvalue' })
+		//await myAddressBookDB.put('MY-DAL', aliceDALDB.address)
+		console.log("myAddressBookDB",myAddressBookDB)
+		myAddressBook = myAddressBookDB.address
+		console.log("myAddressBook now (address)",myAddressBook)
+
+		const allContactDBs = await myAddressBookDB.all()
+		allMyContacts = allContactDBs.map((it, i)=>{
+			const newContact = it
+			newContact.id = i
+			return newContact
+		})
+		let i = 0
+		for (let key in allContactDBs) {
+			if(allContactDBs[key].key==="MY-DAL"){
+				await loadMyDAL("MY-DAL")
+			}
+		}
+
+	}
+	const loadMyDAL = async (dal) => {
+		const aliceDALDB = await orbitdb1.open(dal, { type: 'keyvalue' })
+		await aliceDALDB.put('NAME', "Alice Brown")
+		await aliceDALDB.put('CITY', "Istanbul")
+		allMyAddressFields = await aliceDALDB.all()
+	}
+
+	const loadMyContactFrom = async (e) => {
+		console.log("row clicked",e.detail.key)
+		const dbAddr  = e.detail.value
+		console.log(`connecting to ${dbAddr} `)
+		const syncingDAL = await orbitdb1.open(dbAddr, { type: 'keyvalue' })
+		const allSyncingDAL = await syncingDAL.all()
+		console.log("currentAddress of "+e.detail.key,toObject(allSyncingDAL))
+		currentAddress = toObject(allSyncingDAL)
+	}
+
+	function toObject(arr) {
+		var rv = {};
+		for (var i = 0; i < arr.length; ++i)
+			rv[arr[i].key] = arr[i].value;
+		return rv;
+	}
+
+	const disconnect = async () => {
+		console.log("disconnecting ",myAddressBook)
+		const res = await myAddressBookDB.close()
+		console.log("myAddressBookDB",myAddressBookDB)
+	}
+
+</script>
 
 <section>
 	{#if nodeId}
 		<h3>
 			Node ID: {nodeId}
 		</h3>
-		<h2>
-			IPFS loaded in a Vite app, <strong>right?!</strong>
-		</h2>
-		{#if putted}ipfs.dag.put({JSON.stringify(helloWorld)}, {"{ storeCodec: 'dag-pb' }"})
-			<br />
-			CID verion0: {putted.toV0().toString()}
-			<br />
-			CID verion1: {putted.toString()}
-		{/if}<br /><br />
-		{#if added}ipfs.add({JSON.stringify(helloWorld)})<br />CID v0: {added.cid
-				.toV0()
-				.toString()}{/if}
-		<br /><br />
-		{#if added}ipfs.add(data) >> ipfs.cat(data):<br />
-
-			<a target="_blank" href="https://dweb.link/api/v0/cat?arg={added.cid.toV0().toString()}"
-				>{added.cid.toV0().toString()}</a
-			>
-			<br /> Check that they are the same:
-			<b> {putted.toV0().toString() === added.cid.toV0().toString()}</b>
-		{/if}
-		<br /><br />
-		{#if putWithLinks}Explore Linked Data:<br /><a
-				target="_blank"
-				href="https://explore.ipld.io/#/explore/{putWithLinks.toV0().toString()}"
-				>{putWithLinks.toV0().toString()}</a
-			>
-
-			<br />DWeb Site:<br /><a
-				target="_blank"
-				href="https://{putWithLinks.toV1().toString()}.ipfs.dweb.link/"
-				>https://[cid].ipfs.dweb.link</a
-			><br /><br />
-
-			CloudFlare:<br />
-			<a target="_blank" href="https://{putWithLinks.toV1().toString()}.ipfs.cf-ipfs.com/"
-				>https://[cid].ipfs.cf-ipfs.com</a
-			><br /><br />
+		<TextInput labelText="myAddressBook" placeholder="enter addressbook name or address" bind:value={myAddressBook}/>
+		{#if !myAddressBookDB}
+			<Button on:click={connect}>Connect to addressbook</Button>
+		{:else}
+			<Button on:click={disconnect}>Disconnect {myAddressBookDB.name}</Button>
 		{/if}
 
-		{#if addedWithLinks}Explore Add with Links:<br /><a
-				target="_blank"
-				href="ipfs://bafybeiftcyj7gao3kykwic743wuulwpzkeat4nfmzapbgz5mxsfxsnpbtu/#/explore/{addedWithLinks.cid
-					.toV0()
-					.toString()}">{addedWithLinks.cid.toV0().toString()}</a
-			>
+		<p></p>
+		<p></p>
+		<p></p>
+		<Grid>
+			<Row><Column>My Address:</Column><Column></Column></Row>
+			{#each allMyAddressFields as field}
+				<Row><Column><h3>{field.key} {field.value}</h3></Column></Row>
+			{/each}
+			<Row><Column></Column><Column></Column></Row>
+			<Row><Column></Column><Column></Column></Row>
+			<Row><Column></Column><Column></Column></Row>
+			<Row><Column></Column><Column></Column></Row>
+		</Grid>
+		<p></p>
+		<p></p>
 
-			<br />DWeb Site:<br /><a
-				target="_blank"
-				href="https://{addedWithLinks.cid.toV1().toString()}.ipfs.dweb.link/">View Data</a
-			><br />Mirror:<br />
+		<DataTable
+				on:click:row={(e) => loadMyContactFrom(e)}
+				headers={[
+                    { key: "key", value: "name" },
+                    { key: "value", value: "address" },
+                  ]}
+				bind:rows={allMyContacts}
+		/>
+		<p>&nbsp;</p>
+		<Tile>
+			{currentAddress?.NAME?currentAddress?.NAME:''}
+			{currentAddress?.STREET?currentAddress?.STREET:''}
+			{currentAddress?.CITY?currentAddress?.CITY:''}
+		</Tile>
 
-			<a target="_blank" href="https://{addedWithLinks.cid.toV1().toString()}.ipfs.cf-ipfs.com/"
-				>View Mirror Data</a
-			><br /><br />
-		{/if}
 	{:else}
 		Loading IPFS...
 	{/if}
 </section>
-
-<style>
-	section {
-		display: flex;
-		flex-direction: column;
-		justify-content: center;
-		align-items: center;
-		flex: 1;
-	}
-
-	h1 {
-		width: 100%;
-	}
-</style>

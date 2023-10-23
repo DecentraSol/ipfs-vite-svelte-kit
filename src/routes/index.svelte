508e26c87e1915c4cbed85d6e4821f15f1b8e281
Nico Krause <nico@le-space.de>
2023-10-23 18:48:44 +0500

    this should replicate in two different browsers but it does not

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,19 +1,14 @@
 <script lang="ts">
 	import { onMount } from 'svelte';
-	import {TextInput, Button, DataTable, Grid, Row, Column, Tile} from "carbon-components-svelte";
 
-	let ipfsNode1,ipfsNode2;
-	let nodeId;
+
+	let ipfsNode1;
 	let orbitdb1;
-	let putted, added;
-	let putWithLinks, addedWithLinks;
+	let db;
+	let nodeId;
+	let count;
+	let IPFSAccessController
 
-	let myAddressBook = "/orbitdb/zdpuAqSriPZbh2PWy1m5d7n1ZfzQPPmnGqMM8KGYxBHWgqXEt" //"myAddressBook";
-	let myAddressBookDB;
-
-	let allMyAddressFields = []
-	let allMyContacts = []
-	let currentAddress = {}
 	onMount(async () => {
 		const config1 = {
 			Bootstrap: [],
@@ -34,10 +29,21 @@
 		// In Svelte, a hot module refresh can cause lockfile problems
 		// so we assign the ipfs node to globalThis to avoid lock file issues
 		if (!globalThis.ipfsNode) {
-			// no ipfs saved to globalThis, so load it up
 			const IPFSmodule = await import('../modules/ipfs-core/ipfs-core.js');
 			const IPFS = IPFSmodule.default;
 			ipfsNode1 = await IPFS.create({ config: config1, repo: './ipfs/1' });
+
+			console.log("ipfsNode1",ipfsNode1)
+			//
+			// ipfsNode1.on('peer:discovery', (peer) => {
+			//   console.log('discovered', peer)
+			// })
+			//
+			// ipfsNode1.on('peer:connect', async (peer) => {
+			//   console.log('connected', peer)
+			//
+			// 	ipfsNode1.swarm.peers().then(peers => console.log('current peers connected: ', peers))
+			// })
 			globalThis.ipfsNode1 = ipfsNode1;
 		} else {
 			ipfsNode1 = globalThis.ipfsNode1;
@@ -48,118 +54,73 @@
 
 		const OrbitDBModul = await import('../modules/orbitdb-core/index.js');
 		const createOrbitDB = OrbitDBModul.createOrbitDB;
-		// const ipfsAccessController = OrbitDBModul.IPFSAccessController;
+		IPFSAccessController = OrbitDBModul.IPFSAccessController;
 		orbitdb1 = await createOrbitDB({ ipfs: ipfsNode1 })
+		const dbAddress = "/orbitdb/zdpuAynrr9SX7Q8xL7ZmJw2eKZePdoNE3AXZqLjz7gJEAJin4" //"counter" //"counter"
+		db = await orbitdb1.open(dbAddress, {
+			type: 'keyvalue',
+			sync: true,
+			indexBy: 'id',
+			create: true,
+			//overwrite: false,
+			public: true,
+			AccessController: IPFSAccessController({ write: ['*'] })})
+		console.log("db",db.events)
+		db.events.on("replicated", async (dbAddress, count, newFeed, d) => {
+			console.log("replicated - loading posts from db");
+			// log.msg("dbAddress", dbAddress);
+			// log.msg("count", count);
+			// log.msg("feed", newFeed);
 
-		// //2. Bobs DAL
-		// const bobDAL = "bobDAL"
-		// const bobDALDB = await orbitdb2.open(bobDAL, { type: 'keyvalue' })
-		// await bobDALDB.put('NAME', "Bob Smith")
-		// await bobDALDB.put('STREET', "The Address Downtown")
-		// await bobDALDB.put('CITY', "Dubai")
-		// const allBob = await bobDALDB.all()
-		// console.log("allBob",allBob)
+		});
 
-		//3. My address book
+		db.events.on("write", async (hash, entry, heads) => {
+			console.log("orbit db wrote to ipfs" + hash, entry);
+			// this.addPostToStore(entry);
+		});
 
+		// When the database is ready (ie. loaded), display results
+		db.events.on("ready", (dbAddress, feedReady) => {
+			console.log("database ready " + dbAddress, feedReady);
+			// if(this.feed !== undefined) this.feed.all.map(this.addPostToStore);
+			// else log.danger('feed is still undefined although ready')
+		});
 
-		// const bobsName = await bobDALDB.get("NAME")
-		// let allAliceaddressbook = await myAddressBookDB.all()
-		// console.log("allAliceaddressbook",allAliceaddressbook)
+		db.events.on("replicate.progress", async (dbAddress, hash, obj) => {
+			console.log("replicate.progress", dbAddress, hash);
+			// log.msg('post subject',obj.payload.value.subject)
+			// log.msg('post body',obj.payload.value.body)
+			// if(obj.payload.op==="DEL"){
+			// 	const entryHash = obj.payload.value
+			// 	for (let i = 0; i < this.posts.length; i++) {
+			// 		log.msg("iterating through posts>", this.posts[i].hash, this.posts[i].address);
+			// 		if (this.posts[i].hash === entryHash) {
+			// 			log.success("removed post from store because it was deleted on another node",entryHash);
+			// 			this.posts = [] //empty store because it gets reloaded anyways
+			// 		}
+			// 	}
+			// }
+		});
 
-		//4. Bobs address book (has only his address
-		// const myAddressBook = "myAddressBook"
-		// const bobsAddressBookDB = await orbitdb2.open(myAddressBook, { type: 'keyvalue' })
-		// await bobsAddressBookDB.put('MY-DAL', bobDALDB.address)
-		// const allbobsAddressBookDB = await bobsAddressBookDB.all()
-		// console.log("allbobsAddressBookDB",allbobsAddressBookDB)
+		console.log("db.address",db.address)
+		// await db.close()
+		const dbReopened = await orbitdb1.open(db.address)
 
-		//Now Alice is connecting (scanning) Bobs address book (we need his address not his contacts!)
-		//TODO who gets read access for bobs address book? only bob or everybody with a password or privatekey?
-		// const alice2BobsDAL = await orbitdb1.open(bobDALDB.address, { type: 'keyvalue' })
-		// const allalice2BobsDAL = await alice2BobsDAL.all()
-		// console.log("allalice2BobsDAL",allalice2BobsDAL) //alice orbitdb now contains bobs address
-
-		//we now put bobs dal book into alice addressbook and iterator over all entries in alice address book and print out all DALs
-		// await myAddressBookDB.put(bobsName, bobDALDB.address)
-		// allAliceaddressbook = await myAddressBookDB.all()
-		// console.log("allAliceaddressbook",allAliceaddressbook)
-
-		//now change bobs address (Bob is moving to a new address)
-		// const newAddress = "Jumeirah Beach"
-		// console.log("changing bobs address",newAddress)
-		// await bobDALDB.put('STREET',newAddress)
-
-		// for (let key in allAliceaddressbook) {
-		// 	console.log("obj",allAliceaddressbook[key])
-		// 	const dbAddr = allAliceaddressbook[key].value
-		// 	console.log(`connecting to ${allAliceaddressbook[key].key}'s ${dbAddr} `)
-		// 	const syncingDAL = await orbitdb1.open(dbAddr, { type: 'keyvalue' })
-		// 	const allSyncingDAL = await syncingDAL.all()
-		// 	console.log("allSyncingDAL",allSyncingDAL)
-		// }
+		console.log("dbReopened",dbReopened)
+		count = await db.get('count')
+		if(!count){
+			await db.put('count', 0)
+			count = await db.get('count')
+		}
+		const all = await db.all()
+		console.log("all",all)
 
 		return () => {
 			console.log('the ipfs node is being stopped');
 			ipfsNode1.stop();
-			ipfsNode2.stop();
 			globalThis.ipfsNode1 = null;
-			globalThis.ipfsNode2 = null;
 		};
 	});
-
-	const connect = async () => {
-		console.log("connecting to ",myAddressBook)
-		myAddressBookDB = await orbitdb1.open(myAddressBook, { type: 'keyvalue' })
-		//await myAddressBookDB.put('MY-DAL', aliceDALDB.address)
-		console.log("myAddressBookDB",myAddressBookDB)
-		myAddressBook = myAddressBookDB.address
-		console.log("myAddressBook now (address)",myAddressBook)
-
-		const allContactDBs = await myAddressBookDB.all()
-		allMyContacts = allContactDBs.map((it, i)=>{
-			const newContact = it
-			newContact.id = i
-			return newContact
-		})
-		let i = 0
-		for (let key in allContactDBs) {
-			if(allContactDBs[key].key==="MY-DAL"){
-				await loadMyDAL("MY-DAL")
-			}
-		}
-
-	}
-	const loadMyDAL = async (dal) => {
-		const aliceDALDB = await orbitdb1.open(dal, { type: 'keyvalue' })
-		await aliceDALDB.put('NAME', "Alice Brown")
-		await aliceDALDB.put('CITY', "Istanbul")
-		allMyAddressFields = await aliceDALDB.all()
-	}
-
-	const loadMyContactFrom = async (e) => {
-		console.log("row clicked",e.detail.key)
-		const dbAddr  = e.detail.value
-		console.log(`connecting to ${dbAddr} `)
-		const syncingDAL = await orbitdb1.open(dbAddr, { type: 'keyvalue' })
-		const allSyncingDAL = await syncingDAL.all()
-		console.log("currentAddress of "+e.detail.key,toObject(allSyncingDAL))
-		currentAddress = toObject(allSyncingDAL)
-	}
-
-	function toObject(arr) {
-		var rv = {};
-		for (var i = 0; i < arr.length; ++i)
-			rv[arr[i].key] = arr[i].value;
-		return rv;
-	}
-
-	const disconnect = async () => {
-		console.log("disconnecting ",myAddressBook)
-		const res = await myAddressBookDB.close()
-		console.log("myAddressBookDB",myAddressBookDB)
-	}
-
 </script>
 
 <section>
@@ -167,43 +128,11 @@
 		<h3>
 			Node ID: {nodeId}
 		</h3>
-		<TextInput labelText="myAddressBook" placeholder="enter addressbook name or address" bind:value={myAddressBook}/>
-		{#if !myAddressBookDB}
-			<Button on:click={connect}>Connect to addressbook</Button>
-		{:else}
-			<Button on:click={disconnect}>Disconnect {myAddressBookDB.name}</Button>
-		{/if}
-
-		<p></p>
-		<p></p>
-		<p></p>
-		<Grid>
-			<Row><Column>My Address:</Column><Column></Column></Row>
-			{#each allMyAddressFields as field}
-				<Row><Column><h3>{field.key} {field.value}</h3></Column></Row>
-			{/each}
-			<Row><Column></Column><Column></Column></Row>
-			<Row><Column></Column><Column></Column></Row>
-			<Row><Column></Column><Column></Column></Row>
-			<Row><Column></Column><Column></Column></Row>
-		</Grid>
-		<p></p>
-		<p></p>
-
-		<DataTable
-				on:click:row={(e) => loadMyContactFrom(e)}
-				headers={[
-                    { key: "key", value: "name" },
-                    { key: "value", value: "address" },
-                  ]}
-				bind:rows={allMyContacts}
-		/>
-		<p>&nbsp;</p>
-		<Tile>
-			{currentAddress?.NAME?currentAddress?.NAME:''}
-			{currentAddress?.STREET?currentAddress?.STREET:''}
-			{currentAddress?.CITY?currentAddress?.CITY:''}
-		</Tile>
+		hello our count is <div on:click={
+			async () => {
+				console.log("count is",count)
+				await db.put('count', ++count)
+			}}>{count}</div>
 
 	{:else}
 		Loading IPFS...
